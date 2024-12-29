51ea01a9097bb9ac8331e2cd0146b0d33b0f30a4
Arthur Fernandes Vaz <arthurvaz1805@gmail.com>
2024-12-29 16:40:25 -0300

    Attribute Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,34 +10,11 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'projeto-angular';
-  //passando para propriedade
-  inputText = "Apague e Digite aqui";
-  inputType = "text";
-  isDisabled = false;
+  buttonTitle = 'titulo do botão';
+  buttonDisable = false;
 
-  enableInput(){
-    this.isDisabled = false;
-  }
-  disabledInput(){
-    this.isDisabled = true;
-  }
-  setSenha(){
-    this.inputType = 'password';
-  }
-  setTexto(){
-    this.inputType = 'text';
-  }
-  meuLog(){
-    console.log('logou');
-  }
-  limpar(){
-    this.inputText = ""
-    this.inputType = ""
-  }
-// monitorar e capturar o valor digitado em um campo 
-// de entrada de texto (<input>) sempre que o usuário soltar uma tecla. 
-  handleInputKeyup(event: KeyboardEvent){
-    const textoAtual = (event.target as HTMLInputElement).value
-    console.log(textoAtual);
+  onButonClick(){
+    this.buttonTitle = 'titulo ALTERADOOOO'
+    this.buttonDisable = true;
   }
 }
