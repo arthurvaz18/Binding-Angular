0c50abcff9b31371925d126470962427d90d9593
Arthur Fernandes Vaz <arthurvaz1805@gmail.com>
2025-01-03 10:53:02 -0300

    CSS Class Binding Part 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,31 +9,9 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'projeto-angular';
-  widthButton1 = '100px';
-  widthButton2 = '100px';
-  widthButton3 = '100px';
-  cor = 'white';
 
-  stylesObj={
-    width: '100px',
-    backgroundColor:'grey',
-  }
-  updateStyleObj(){
-    console.log('updateStyleObj');
+  botaoVermelho = true;
+  botaoVerde = true;
 
-    this.stylesObj.width = '170px';
-    this.stylesObj.backgroundColor = 'lightblue';
-  }
-  updateStyleObjCorrect(){
-    console.log('updateStyleOnjCorrect');
-
-    this.stylesObj ={
-      width: '170px',
-      backgroundColor: 'lighblue'
-    };
-  }
-  updateWidth(){
-    this.widthButton2 = '200px';
-  }
+  
   }
