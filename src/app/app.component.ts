76c5279bf1a12331cf7da33842610681c16bad59
Arthur Fernandes Vaz <arthurvaz1805@gmail.com>
2025-01-12 19:04:05 -0300

    Two Way Data Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppFilhoComponent } from "./app-filho/app-filho.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AppFilhoComponent],
+  imports: [RouterOutlet, AppFilhoComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+name: string = 'Arthur';
 
-  botaoVermelho = true;
-  botaoVerde = true;
-
-  informacao?: string;
-  InformacaoDoFilho?: string;
-
-  getInformacao(){
-    this.informacao = 'Informacao do pai para o filho';
-  }
-  recebendoInformacaoDoFilho(event: string): void{
-      this.InformacaoDoFilho = event;
-  }
+manipulando(event: any){
+  console.log(event);
+  this.name = (event);
+  
+}
   }
