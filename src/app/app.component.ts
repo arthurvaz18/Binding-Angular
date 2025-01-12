d7859bcaaf321703b578c6265e278f82d35675a9
Arthur Fernandes Vaz <arthurvaz1805@gmail.com>
2025-01-12 19:47:27 -0300

    Diretiva Estrutural ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,20 +2,50 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppFilhoComponent } from "./app-filho/app-filho.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AppFilhoComponent, FormsModule],
+  imports: [RouterOutlet, AppFilhoComponent, FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-name: string = 'Arthur';
+minhaProp =true;
+pessoa: any ={
+  status:1,
+  name: undefined,
+}
 
-manipulando(event: any){
-  console.log(event);
-  this.name = (event);
+toggleProp(){
+  this.minhaProp = !this.minhaProp;
+}
+istruthy(){
+  //return {};
+  //return [];
+  //return true;
+  //return 1;
+  //return "teste";
+  //return 'teste';
+  //return "0";
+  //return "false";
+  //return "this.pessoa.status";
   
 }
+
+isfalse(){
+  //return false;
+  //return 0;
+  //return "";
+  //return '';
+  //return null;
+  //return "teste";
+  //return undefined;
+  //return "-0";
+  //return NaN;
+  //return this.pessoa.name;
+  
+}
+
   }
