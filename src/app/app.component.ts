88cf38858decc78389c0f0713c3b3bd0ff81293c
Arthur Fernandes Vaz <arthurvaz1805@gmail.com>
2025-01-13 00:13:02 -0300

    Realizando Pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,22 +4,27 @@ import { FormsModule } from '@angular/forms';
 import { AppFilhoComponent } from "./app-filho/app-filho.component";
 import { CommonModule } from '@angular/common';
 import { PessoaComponent } from './pessoa/pessoa.component';
+import { StatusClassPipe } from './pipes/status-class.pipe';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AppFilhoComponent, PessoaComponent, FormsModule, CommonModule],
+  imports: [RouterOutlet, AppFilhoComponent, PessoaComponent, StatusClassPipe, FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  isGreen: boolean = true;
+  pessoa = {
+    name:'Arthur',
+    status: 1,
+  };
+  pessoa2 = {
+    name:'Aniltamar',
+    status: 2,
+  };
+  pessoa3 = {
+    name:'Alefe',
+    status: 3,
+  };
 
-  setToGreen(){
-    this.isGreen = true;
-  }
-
-  setToOrange(){
-    this.isGreen = false;
-  }
 }
---
+++ src/app/pipes/status-class.pipe.ts
@@ -0,0 +1,27 @@
+import { Pipe, PipeTransform } from "@angular/core";
+@Pipe({
+    name:'statusClass',
+    standalone: true, // Indica que o pipe é standalone
+})
+
+export class StatusClassPipe implements PipeTransform {
+    transform(status: number): string {
+        console.log('StatusClassPipe');
+        //if(status === 1)
+        //   return 'active';
+        //else if(status === 2) {
+        //    return 'partial';
+        //}
+        //else {
+        //    return 'blocked';
+        //}
+
+        const obj: {[key: number]: string } = {
+            1: 'active',
+            2: 'partial',
+            3: 'blocked',
+        }
+        return obj [status];
+    }
+
+}
